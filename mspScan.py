d3e6bdb55be19602d31d3a64f762cbc2b042f94c
Sean Fackler <sfackler@umd.edu>
2016-04-01 14:25:53 -0700

    Update mspScan.py
    downloaded from e-mail March 21, 2016

--- mspScan.py
+++ mspScan.py
@@ -12,6 +12,10 @@ import os.path
 import numpy as np
 	
 
+def round_col(data_f, colname, digits=0):
+	if colname in data_f.columns:
+		data_f[colname] = data_f[colname].round(digits)
+
 
 
 def dissect_filename(filename):
@@ -77,15 +81,27 @@ def prepare_scan(scandata_f, datacounter = "Counter 1", reference_counter = 'Cou
 		print("No counter for clock found (looked for 'Counter 4' and 'Counter 6'). Defaulting to 'Counter 0'.")
 		clockname	= 'Counter 0'
 
-#	if 'Magnet Field.1' in scandata_f.columns:
-#		scandata_f['Magnet Field'] = scandata_f['Magnet Field.1']
+	if 'Magnet Field.1' in scandata_f.columns:
+		scandata_f['Magnet Field'] = scandata_f['Magnet Field.1']
 #		print("Overwriting 'Magnet Field' with 'Magnet Field.1'")
 		
 	scandata_f["I_Norm0"]	= scandata_f[datacounter].astype(float) / scandata_f[reference_counter].astype(float) 
 	scandata_f["I_Normt"]	= scandata_f[datacounter].astype(float) / scandata_f[clockname].astype(float) 
-	scandata_f["Energy"] 	= scandata_f["Energy"].round(1)
-#	scandata_f["Y"] 	= scandata_f["Y"].round(2)
-	scandata_f["Z"] 	= scandata_f["Z"].round(2)
+	round_col(scandata_f, "Energy", 1)
+	round_col(scandata_f, "Aperture Position", 3)
+#	scandata_f["Aperture Position"] 	= scandata_f["Aperture Position"].round(3)
+#	scandata_f["M2 Bend UP"] 	= scandata_f["M2 Bend UP"].round(3)
+#	scandata_f["M2 Bend DN"] 	= scandata_f["M2 Bend DN"].round(3)
+
+#	scandata_f["count_x"] = (scandata_f.index.astype(int)/480)
+#	scandata_f["count_x"] = (scandata_f["count_x"]/6).astype(int)
+#	scandata_f["count_y"] = (scandata_f.index.astype(int)/480)
+#	scandata_f["count_y"] %= 6
+
+	round_col(scandata_f, "X", 2)
+	round_col(scandata_f, "Y", 2)
+	round_col(scandata_f, "Z", 2)
+	round_col(scandata_f, "Theta", 1)
 #	scandata_f["T"] 	= 20*(((scandata_f["Temperature A"]-10)/20).round())
 	return(scandata_f)
 
@@ -100,10 +116,18 @@ def read_scans(filenames, datacounter = "Counter 1", reference_counter = 'Counte
 	return (scandata_f)
 
 def sort_zy_lines(a,b):
-	if 	a[0]>b[0]: return  1
+	if	len(a) <1: return  0
+	elif 	a[0]>b[0]: return  1
 	elif	a[0]<b[0]: return -1
+	elif	len(a) <2: return  0
 	elif	a[1]>b[1]: return  1
 	elif	a[1]<b[1]: return -1
+	elif	len(a) <3: return  0
+	elif	a[2]>b[2]: return  1
+	elif	a[2]<b[2]: return -1	
+	elif	len(a) <4: return  0
+	elif	a[3]>b[3]: return  1
+	elif	a[3]<b[3]: return -1	
 	else: return 0
 
 
